03227fca473e1374b8d8a6ed2c5174c39da4f866
Alfred <alfredchen@gameislive.com>
2025-09-04 11:48:47 +0800

    * Update basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
   },
   // GitHub Pages会部署到子路径，如果你的仓库名不是 username.github.io
   // 需要设置 basePath，请根据你的仓库名修改
-  // basePath: '/guitarChord',
-  // assetPrefix: '/guitarChord/',
+  basePath: '/guitarChord',
+  assetPrefix: '/guitarChord/',
 }
 
 module.exports = nextConfig
